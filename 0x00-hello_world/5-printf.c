553a73cf0a0821565e1a079a65ca4fb87bbe95b0
OctaveC <octavec@outlook.com>
2021-02-03 13:39:59 +0000

    Exo 5 first draft

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - This function doesn't take an argument, so it takes a (void)
+ *
+ * Return: This program return the number 0
+ */
+int main(void)
+{
+printf("with proper grammar\, but the outcome is a piece of art\,\n");
+return (0);
+}
