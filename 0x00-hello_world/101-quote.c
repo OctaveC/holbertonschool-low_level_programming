285a275121386d8db4a49835e4eb00e79728f6eb
OctaveC <octavec@outlook.com>
2021-02-03 15:41:41 +0000

    forgot about the other header

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,11 @@
 #include<stdio.h>
+#include <unistd.h>
 /**
  * my_function - This is a function that takes no argument, so it is void
  *
  * Return: This returns 1
  */
-main(void)
+int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
