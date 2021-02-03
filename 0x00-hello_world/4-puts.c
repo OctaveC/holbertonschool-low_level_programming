051d362aa9b59f79972c99abf519cf43f7f81d2b
OctaveC <octavec@outlook.com>
2021-02-03 13:22:06 +0000

    Hello, puts first draft

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - this is function uses (void) because it doesn't take arguments
+ *
+ * Return: This thingy returns 0
+*/
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
