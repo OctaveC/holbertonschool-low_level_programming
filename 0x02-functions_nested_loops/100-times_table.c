71838f47686571df00b58e0b44302196ee1fbf91
OctaveC <octavec@outlook.com>
2021-02-08 17:51:23 +0000

    first exo bonus done

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,53 @@
+#include "holberton.h"
+
+/**
+ * times_table - print times table for n
+ *
+ *
+ * Return: 0
+ */
+void print_times_table(int n)
+{
+	int t, p, m;
+
+	if ((n == 0) || (n >= 15))
+	{
+		return;
+	}
+	for (t = 0; t <= n; t++)
+	{
+		for (p = 0; p <= n; p++)
+		{
+			m = t * p;
+			if (p == 0)
+			{
+			_putchar(p + '0');
+			}
+			else if (m <= 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(m + '0');
+			}
+			else if (m <= 99)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(m / 10 + '0');
+				_putchar(m % 10 + '0');
+			}
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(m / 100 + '0');
+				_putchar((m % 100) / 10 + '0');
+				_putchar(m % 10 + '0');
+			}
+		}
+		_putchar('\n');
+	}
+}
