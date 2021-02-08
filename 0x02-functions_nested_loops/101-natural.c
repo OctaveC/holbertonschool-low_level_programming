719a1d5e01ba7c068976db20ef56006c69e32cae
OctaveC <octavec@outlook.com>
2021-02-08 18:15:40 +0000

    exo bonus 2 done

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - print the sum of the natural nums under 1024
+ *
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int num, tot = 0;
+
+	for (num = 0; num < 1024; num++)
+		{
+			if (num % 3 == 0 && num % 5 == 0)
+				tot += num;
+		}
+	printf("%i\n", tot);
+}
