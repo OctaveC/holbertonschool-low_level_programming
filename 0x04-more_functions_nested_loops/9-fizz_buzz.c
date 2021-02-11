7d4901034546e652853633b148e1d4856dc9734e
OctaveC <octavec@outlook.com>
2021-02-11 13:22:28 +0000

    finished fibu

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ * main - replaces some numsbers by fizz or buzz or fizzbuzz
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int c;
+
+	for (c = 1; c <= 100; c++)
+	{
+		if ((c % 3 == 0) && (c % 5 == 0))
+		{
+			printf("FizzBuzz");
+		}
+		else if (c % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (c % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else
+		{
+			printf("%d", c);
+		}
+		if (c < 100)
+			printf(" ");
+	}
+	printf("\n");
+	return (0);
+}
