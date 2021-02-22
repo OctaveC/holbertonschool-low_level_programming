fd68812717e243cb320b5288d62b290e75d3a309
OctaveC <octavec@outlook.com>
2021-02-22 16:40:43 +0000

    exo 2 done

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+#include <stddef.h>
+/**
+ * _strchr - Locate a character in a string.
+ * @s: the string we're searching
+ * @c: the char we attempt to find
+ *
+ * Return: First occurence of the character, or NULL
+ */
+
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (c == *s)
+		{
+			return (s);
+		}
+		s++;
+	}
+	return (NULL);
+}
