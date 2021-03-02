006d451d4456ab30d9f4db761c76544ddb66fc8a
OctaveC <octavec@outlook.com>
2021-03-02 18:11:23 +0000

    trying out exo 5

---
+++ 0x0B-malloc_free/5-argstostr.c
@@ -0,0 +1,59 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * argstostr - Concatenates all arguments.
+ * @ac: Numbers of arguments
+ * @av: Pointer to array of arguments
+ *
+ * Return: A pointer to a new string
+ */
+char *argstostr(int ac, char **av)
+{
+	char *arguments;
+	int size = 0, ite1, ite2, ite3;
+
+	if (ac == 0 || av == NULL)
+	{
+		return (NULL);
+	}
+	for (ite1 = 0; ite1 < ac; ite1++)
+	{
+		size = size + _strlen(av[ite1]);
+	}
+
+	arguments = malloc((size) * sizeof(char));
+	if (arguments == NULL)
+		return (NULL);
+
+	for (ite1 = 0; ite1 < ac; ite1++)
+	{
+		for (ite2 = 0; av[ite1][ite2] != '\0'; ite2++)
+		{
+			arguments[ite3] = av[ite1][ite2];
+			ite3++;
+		}
+		arguments[ite3++] = '\n';
+	}
+	arguments[ite3] = '\0';
+	return (arguments);
+}
+
+
+/**
+ * _strlen - Return the length of a string
+ * @s: a pointer
+ *
+ * Return: The length of a string
+ */
+int _strlen(char *s)
+{
+	int n;
+
+	while (s[n] != '\0')
+	{
+		n++;
+	}
+	return (n);
+}
