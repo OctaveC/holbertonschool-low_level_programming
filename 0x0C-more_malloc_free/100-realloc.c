591d79ae731225c12c002a7b4dc9278c0107722e
OctaveC <octavec@outlook.com>
2021-03-04 17:19:01 +0000

    forgot about returning ptr

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -17,14 +17,14 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	if (new_size == old_size)
 		return (ptr);
 
-	if (ptr != NULL && new_size == 0)
-	{
-		free(ptr);
-		return (NULL);
-	}
 	if (ptr == NULL)
 	{
 		ptr = malloc(new_size);
+		return (ptr);
+	}
+	else if (new_size == 0)
+	{
+		free(ptr);
 		return (NULL);
 	}
 	free(ptr);
