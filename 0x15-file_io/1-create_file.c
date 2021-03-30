3280c829c6d8cc2eeba0627ec41be7a9d8e7b803
OctaveC <octavec@outlook.com>
2021-03-30 13:46:16 +0000

    test

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,7 +14,10 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	while (text_content[count])
+	if (!text_content)
+		text_content = "";
+
+	while (text_content[count] != '\0')
 		count++;
 
 	opening = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
