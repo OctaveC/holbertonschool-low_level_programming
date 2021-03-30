149569d7e4e1a099d2e2021869dfbd8306d3ed9c
OctaveC <octavec@outlook.com>
2021-03-30 13:28:25 +0000

    test condition text_content

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,11 +17,11 @@ int create_file(const char *filename, char *text_content)
 	while (text_content[count] != '\0')
 		count++;
 
-	opening = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	opening = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 	if (opening == -1)
 		return (-1);
 
-	if (count != 0)
+	if (text_content != NULL)
 	{
 		writing = write(opening, text_content, count);
 		if (writing == -1)
