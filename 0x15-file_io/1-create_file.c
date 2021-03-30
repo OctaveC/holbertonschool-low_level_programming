7c832278a391a73ffa2fd8b9b63d24fb8218126b
OctaveC <octavec@outlook.com>
2021-03-30 13:38:51 +0000

    tet

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,14 +14,14 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	while (text_content[count] != '\0')
+	while (text_content[count])
 		count++;
 
 	opening = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 	if (opening == -1)
 		return (-1);
 
-	if (text_content != NULL)
+	if (count != 0)
 	{
 		writing = write(opening, text_content, count);
 		if (writing == -1)
