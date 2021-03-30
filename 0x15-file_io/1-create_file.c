55b30f780860d9f7590e8c327a5f03ec235de6e9
OctaveC <octavec@outlook.com>
2021-03-30 13:24:28 +0000

    tried changing order

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,7 +17,7 @@ int create_file(const char *filename, char *text_content)
 	while (text_content[count] != '\0')
 		count++;
 
-	opening = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+	opening = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	if (opening == -1)
 		return (-1);
 
