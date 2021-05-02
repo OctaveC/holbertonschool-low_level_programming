d223b80dc5541c2be99fa9441da4bca41ab76415
OctaveC <octavec@outlook.com>
2021-05-02 14:35:42 +0000

    test

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -19,7 +19,7 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 
 	h->n = n;
 	h->next = *head;
-	h->prev = *head;
+	h->prev = NULL;
 	if (*head != NULL)
 		(*head)->prev = h;
 	*head = h;
