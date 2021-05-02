45d4b45fe7ba3dc5829d228c06ab2ce6905ecdbb
OctaveC <octavec@outlook.com>
2021-05-02 15:19:07 +0000

    test

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -21,7 +21,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	else if (index == 0)
 	{
 		*head = (*head)->next;
-		/*	(*head)->prev == NULL; */
+		(*head)->prev = NULL;
 		free(target);
 		return (1);
 	}
