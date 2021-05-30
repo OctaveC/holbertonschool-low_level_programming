2d18cde9d7e7ceb478f1a65abcf3ed838273a9a3
OctaveC <octavec@outlook.com>
2021-05-30 19:17:47 +0000

    finished exo 6...?

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,30 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_delete - Deletes a hash table.
+ *
+ * @ht: the hash table we want to delete
+ * Return: Nothing.
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	hash_node_t *table, *placeholder;
+	unsigned long int ite;
+
+	if (ht == NULL)
+		return;
+
+	for (ite = 0; ite <= ht->size; ite++)
+	{
+		for (table = ht->array[ite]; table != NULL; table = placeholder)
+		{
+			placeholder = table->next;
+			free(table->value);
+			free(table->key);
+			free(table);
+		}
+	}
+
+        free(ht->array);
+	free(ht);
+}
