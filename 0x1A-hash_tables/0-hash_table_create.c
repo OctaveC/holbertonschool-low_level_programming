495bfef437854a085fc0b602be51ec7fd94c1a15
OctaveC <octavec@outlook.com>
2021-05-30 18:15:08 +0000

    finished exo 0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -17,7 +17,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	if (new_table == NULL)
 		return (NULL);
 
-	new_table->array = malloc(size * sizeof(hash_node_t));
+	new_table->array = calloc(size, sizeof(hash_node_t));
 	if (new_table->array == NULL)
 	{
 		free(new_table);
