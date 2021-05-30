c932676cdf5e48a4c411889ba4161c242ba75d24
OctaveC <octavec@outlook.com>
2021-05-30 18:19:36 +0000

    finished exo 100

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -10,11 +10,14 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *new_table;
 
+	if (size == 0)
+		return (NULL);
+
 	new_table = malloc(sizeof(hash_table_t));
 	if (new_table == NULL)
 		return (NULL);
 
-	new_table->array = malloc(size * sizeof(hash_node_t));
+	new_table->array = malloc(size * sizeof(hash_table->array));
 	if (new_table->array == NULL)
 
 	{
